49ba51e40d7513caa5b8d1b3704f84ca9451cbb0
minhtrung3920 <trung392dg@gmail.com>
2019-10-01 15:25:26 +0700

    4_B

---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConstraint5()
+        {
+            int[] arr = new int[] {1,2,3,4,6,5};
+            bool result = Program.IsConstraint5(arr);
+            Assert.AreEqual(true, result);
+            
+        }
+       
+    }
+}
